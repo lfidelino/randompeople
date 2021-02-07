5c222d2581628209b6560a91ba12dc236c9e425c
Lawrence Fidelino <lfidelino@gmail.com>
2021-02-08 05:08:53 +0800

    Set TODOs

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <>
+      {/* TODO Header */}
+      {/* TODO SearchInput */}
+      {/* TODO CardList > Card */}
+      {/* TODO Footer */}
+    </>
+  );
+};
+
+export default App;
